a8e8179ed6abfc88f9945645bae1316998d86237
Ivan Milosavljevic (TheJavaGuy) <TheJavaGuy@yandex.com>
2026-06-11 10:48:17 +0200

    add astro-iconset

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
+import iconset from 'astro-iconset';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astrofy-template.netlify.app',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), iconset()],
   vite: {
     plugins: [tailwindcss()],
   },
